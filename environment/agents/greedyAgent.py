b830d7403c74389edb3a2ca425485af4ac4995cc
stra3 <jelle.arts@outlook.com>
2020-12-04 12:56:49 +0100

    greedy agent implementation and agent switching

--- environment/agents/greedyAgent.py
+++ environment/agents/greedyAgent.py
@@ -1,18 +1,31 @@
 import agents.agent 
 from pygame.locals import (K_UP,K_DOWN,K_RIGHT,K_LEFT, KEYDOWN)
 import pygame.event 
+from numpy import inf
 
-class manualAgent(agents.agent.agent):
+class greedyAgent(agents.agent.agent):
     """
     manually controlled agent
     """
 
+
     def __init__(self,pos,size):
         super().__init__(pos,size) 
+        self.moves = [[-1,0], [0,-1], [1,0], [0,1]] 
+        self.name = "greedy"
     
     def step(self, move):
         self.update_pos(move)
 
     def generateMove(self, obs):
-        
-        return move
+        goal, obstacles = obs
+
+        best_move = [0,0]
+        min_dist = inf
+        for move in self.moves:
+            dist = self.dist_goal(goal, [self.pos[0]+move[0], self.pos[1]+move[1]])
+            if dist < min_dist:
+                best_move = move
+                min_dist = dist
+
+        return best_move
